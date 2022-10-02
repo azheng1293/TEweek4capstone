949646e2f20cb70eb01d963cda03c31d9b539009
John Fulton <john@techelevator.com>
2022-10-02 12:49:31 -0400

    initial commit

---
+++ mini-capstone/Capstone/Program.cs
@@ -0,0 +1,15 @@
+﻿using Capstone.Classes;
+using System;
+
+namespace Capstone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInterface consoleInterface = new UserInterface();
+            consoleInterface.Run();
+
+        }
+    }
+}
